49ab8a1d1aa0de4e102c983c11e7f2383bd03a7a
Yuliia <50263234+Yuliia010@users.noreply.github.com>
2023-09-16 16:50:46 +0300

    new

---
+++ Pract_15092023/Pract_15092023/Program.cs
@@ -0,0 +1,32 @@
+﻿using Pract_15092023.DAL.Repositories;
+
+namespace Pract_15092023
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var repository = new Repository();
+            var students = repository.GetAllStudents();
+            var studentsCards = repository.GetAllStudentCards();
+
+            foreach (var cards in studentsCards)
+            {
+
+            }
+
+            foreach ( var student in students )
+            {
+                Console.WriteLine(
+                    $@"Student Id: {student.Id}
+            Name: {student.Name} {student.LastName},
+            DateBirth: {student.BirthDate},
+            Mail: {student.MailAddress},
+            Phone: {student.PhoneNumber},
+            StudentCard: {student.StudentCard.CardNumber}, exp.date: {student.StudentCard.ExpireDate}");
+
+            }
+        }
+    }
+}
